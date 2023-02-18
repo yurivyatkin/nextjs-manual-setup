9e1a4dfa492ddd30e2ca3d538a134dd706549abb
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2023-02-19 00:19:25 +1300

    Try Material UI

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h4" component="h1" gutterBottom>
+        Welcome to Next.js!
+      </Typography>
+    </Container>
+  );
 }
 
-export default HomePage
-
+export default HomePage;
